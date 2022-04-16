fc8bf445db0480de901b22c5fc270468f9b34035
Dima <nari2377@gmail.com>
2022-04-16 10:47:12 -0700

    add greedy function to optimize.py file

--- NutritionAnalysisTour/optimize.py
+++ NutritionAnalysisTour/optimize.py
@@ -148,3 +148,46 @@ drink_menu = build_menu(name_list, cal_list, caff_list, fat_list, sug_list)
 # for item in drink_menu:
 #     print(item)
 
+
+# Function: greedy 
+# Description: generates the maximized 'best' drinks one can take, based on available 
+#              calories and preferred value (sugar, caffeine, etc.) 
+# Parameters: 
+#     drink_menu:   a list of Drink objects
+#     max_cals:     an integer >= 0, representing maximum available calories to take
+#     key_function: getter method call to Drink, to define ordering of Drink objects 
+#                   by what is considered the 'best' feature (e.g. caffeine, sugars)
+# Returns: None
+def greedy(drink_menu, max_cals, key_function):
+    # Use sorted fxn to create a copy of the items list
+    # Specify key so sort is based on what to consider as the best feature
+    # Then reverse the copied list to sort best to worst
+    drink_menu_copy = sorted(drink_menu, key = key_function, reverse = True)
+    taken_items = []
+    total_cal, total_caff, total_fat, total_sug = 0, 0, 0, 0
+    
+    # For each sorted Drink object, going best to worst...
+    for i in range(len(drink_menu_copy)):
+        # Call get_cal method to get calories 
+        # If these calories, plus total calories are less than or equal to max_cal,
+        # (e.g. is there room to take item?), add to taken_items and add values to running totals
+        # If greater than max_cal value, go to next Drink obj
+        if (total_cal + drink_menu_copy[i].get_cal()) <= max_cals:
+            taken_items.append(drink_menu_copy[i])
+            total_cal  += drink_menu_copy[i].get_cal()          
+            total_caff += drink_menu_copy[i].get_caff()
+            total_fat  += drink_menu_copy[i].get_fat()
+            total_sug  += drink_menu_copy[i].get_sug()
+    
+    # Output results
+    for item in taken_items:
+        print('   ', item)
+    print('         ')
+    print('Total drink %DV for...')
+    print('Calories:', total_cal)
+    print('Caffeine:', total_caff)
+    print('Fats:    ', total_fat)
+    print('Sugars:  ', total_sug)
+    print('*************')
+    
+# greedy(drink_menu, 10, Drink.get_caff)
