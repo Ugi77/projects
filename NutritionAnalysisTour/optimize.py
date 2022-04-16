c8c1523e0b122456a90803bcc89b206df4313ee3
Dima <nari2377@gmail.com>
2022-04-16 10:45:53 -0700

    add annotation to optimize.py file

--- NutritionAnalysisTour/optimize.py
+++ NutritionAnalysisTour/optimize.py
@@ -1,3 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+Author: Dima (Ugi77)
+
+Description: 
+This code inputs a prepared file of drink names & nutritional values, and 
+creates Drink objects to hold this info.  Then, the maxiumum drinks one can 
+obtain (with a given calorie constraint) is output, based upon which value is 
+considered optimal: calories, caffeine, fats, or sugars.
+
+Note: 
+This code was adapted from the MITx 6.00.2x Introduction to Computational Thinking 
+and Data Science course.  The basic structure originates from a lecture example, 
+and this was then modified to input a file, optimize a set of data with different 
+parameters, and output results in a new way.
+
+How to play:
+
+"""
+
 import csv
 
 filename = open('sb_prep.csv', 'r')
@@ -9,8 +30,8 @@ caff_list = []
 fat_list  = []
 sug_list  = []
  
-# iterate over each .csv row, append values to lists
-# cast nutritional values as floats
+# Iterate over each .csv row, append values to lists
+# Cast nutritional values as floats, then integers
 for col in file:
     name_list.append(col['prep_name'])
     cal_list.append(int(float(col['calories_%dv'])))
@@ -24,27 +45,28 @@ for col in file:
 # print('Fat:', fat_list)
 # print('Sugars:', sug_list)
 
+
 # Class: Drink
-# Description: represents aspects of a drink item
+# Description: represents nutritional values of a drink item
 class Drink(object):
     
     # Method: __init__
     # Description: constructor
     # Parameters:
-    #   name: type string, a drink
-    #   cal:  type float, %DV calories in a drink 
-    #   caff: type float, %DV caffeine in a drink
-    #   fat:  type float, %DV fat in a drink 
-    #   sug:  type float, %DV sugars in a drink        
+    #   name: a string, the drink name, size, and milk type
+    #   cal:  an integer, the %DV calories in a drink 
+    #   caff: an integer, the %DV caffeine in a drink
+    #   fat:  an integer, the %DV fats in a drink 
+    #   sug:  an integer, the %DV sugars in a drink        
     # Precondition: none
     # Returns: A newly created object of type Drink
     def __init__(self, name, cal, caff, fat, sug):
         # Data attributes: 
-        #   name: type string, a drink
-        #   cal:  type float, %DV calories in a drink 
-        #   caff: type float, %DV caffeine in a drink
-        #   fat:  type float, %DV fat in a drink 
-        #   sug:  type float, %DV sugars in a drink 
+        #   name: a string, the drink name, size, and milk type
+        #   cal:  an integer, the %DV calories in a drink 
+        #   caff: an integer, the %DV caffeine in a drink
+        #   fat:  an integer, the %DV fats in a drink 
+        #   sug:  an integer, the %DV sugars in a drink 
         self.name = name
         self.cal  = cal
         self.caff = caff
@@ -56,7 +78,7 @@ class Drink(object):
     # Parameters: none
     # Preconditions: none 
     # Postcondition: no change
-    # Returns: a float         
+    # Returns: an integer         
     def get_cal(self):
         return self.cal
 
@@ -65,7 +87,7 @@ class Drink(object):
     # Parameters: none
     # Preconditions: none 
     # Postcondition: no change
-    # Returns: a float    
+    # Returns: an integer   
     def get_caff(self):
         return self.caff
 
@@ -74,7 +96,7 @@ class Drink(object):
     # Parameters: none
     # Preconditions: none 
     # Postcondition: no change
-    # Returns: a float    
+    # Returns: an integer    
     def get_fat(self):
         return self.fat
 
@@ -83,7 +105,7 @@ class Drink(object):
     # Parameters: none
     # Preconditions: none 
     # Postcondition: no change
-    # Returns: a float         
+    # Returns: an integer        
     def get_sug(self):
         return self.sug
     
@@ -94,34 +116,35 @@ class Drink(object):
     # Postcondition: no change
     # Returns: a string    
     def __str__(self):
-        # return self.name + ' %DV: ' + str(self.cal) + ' cal, ' + str(self.caff) + ' caff, ' + str(self.fat) + ' fat, ' + str(self.sug) + ' sug' 
         return self.name
-
-# End of class Drink
 
 # test = Drink("expresso", 3, 4, 5, 6)
 # result = test.get_sug()
 # print(test)
 # print(result)
 
+## END OF CLASS DRINK ##
+
+
 # Function: build_menu 
-# Description: takes in lists, generates a list of Drink objects
+# Description: takes in lists of drink features, generates a list of Drink objects
 # Parameters: 
-#     name_list: a list of strings, of drink names
-#     cal_list:  a list of floats, of calorie %DV values
-#     caff_list: a list of floats, of caffeine %DV values
-#     fat_list:  a list of floats, of fat %DV values
-#     sug_list:  a list of floats, of sugar %DV values
+#     name_list: a list of strings, of drink names, sizes, and milk types
+#     cal_list:  a list of integers, of calorie %DV values
+#     caff_list: a list of integers, of caffeine %DV values
+#     fat_list:  a list of integers, of fat %DV values
+#     sug_list:  a list of integers, of sugar %DV values
 # Returns: a list of Drink objects
 def build_menu(name_list, cal_list, caff_list, fat_list, sug_list):
     menu = []
     for i in range(len(name_list)):
-        # create a Drink object with a list element from name_list, cal_list, etc. at iterated index position
-        # add each Drink object to the menu list
-        drink_obj = Drink(name_list[i], cal_list[i], caff_list[i], fat_list, sug_list[i])
+        # Create a Drink object with a list element from name_list, cal_list, etc. at iterated index position
+        # Add each Drink object to the menu list
+        drink_obj = Drink(name_list[i], cal_list[i], caff_list[i], fat_list[i], sug_list[i])
         menu.append(drink_obj)
     return menu
 
 drink_menu = build_menu(name_list, cal_list, caff_list, fat_list, sug_list)
-for item in drink_menu:
-    print(item)
+# for item in drink_menu:
+#     print(item)
+
